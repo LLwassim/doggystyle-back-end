73a994edaa8abc54017006d08c3fc83f9c6a25cc
blake <blakeeh723@gmail.com>
2019-11-18 15:55:12 -0500

    Perparing Merge Branches Wassim

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const Dog = require('./db/models/Dog');
 app.use(cors());
 app.use(parser.json());
 
-app.get('/', (req, res) => {
+// get all breeds
+app.get('/breeds', (req, res) => {
   DogBreed.find({}).then(dogBreeds => {
     res.json(dogBreeds);
   });
 });
 
-app.get('/breed/:breed', (req, res) => {
+// get breeds by breed
+app.get('/breeds/breed/:breed', (req, res) => {
   DogBreed.find({
     name: { $regex: req.params.breed, $options: 'i' }
   }).then(dogBreeds => {
@@ -21,7 +23,8 @@ app.get('/breed/:breed', (req, res) => {
   });
 });
 
-app.get('/temperament/:temperament', (req, res) => {
+// get breeds by temparament
+app.get('/breeds/temperament/:temperament', (req, res) => {
   DogBreed.find({
     temperament: { $regex: req.params.temperament, $options: 'i' }
   }).then(dogBreeds => {
@@ -29,7 +32,22 @@ app.get('/temperament/:temperament', (req, res) => {
   });
 });
 
-app.post('/', (req, res) => {
+// get all dogs
+app.get('/dogs', (req, res) => {
+  Dog.find({}).then(dogs => {
+    res.json(dogs);
+  });
+});
+
+// create dog
+app.post('/dogs/create', (req, res) => {
+  Dog.create(req.body).then(dog => {
+    res.json(dog);
+  });
+});
+
+// update dog
+app.put('/dogs', (req, res) => {
   Dog.create(req.body).then(dog => {
     res.json(dog);
   });
