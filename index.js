b7ffb7bdb6e29781d6b6f818473f2beaaee1d492
blake <blakeeh723@gmail.com>
2019-11-19 16:46:05 -0500

    ids are the same now holy shit

--- index.js
+++ index.js
@@ -8,59 +8,39 @@ const User = require('./db/models/User');
 app.use(cors());
 app.use(parser.json());
 
+/* BREEDS routes */
 // get all breeds
 app.get('/breeds', (req, res) => {
-  DogBreed.find({}).then(dogBreeds => {
+  DogBreed.find({})
+  .then(dogBreeds => {
     res.json(dogBreeds);
   });
 });
-
 // get breeds by breed
 app.get('/breeds/breed/:breed', (req, res) => {
-  DogBreed.find({
-    name: { $regex: req.params.breed, $options: 'i' }
-  }).then(dogBreeds => {
+  DogBreed.find({name: { $regex: req.params.breed, $options: 'i' }})
+  .then(dogBreeds => {
     res.json(dogBreeds);
   });
 });
-
 // get breeds by temparament
 app.get('/breeds/temperament/:temperament', (req, res) => {
-  DogBreed.find({
-    temperament: { $regex: req.params.temperament, $options: 'i' }
-  }).then(dogBreeds => {
+  DogBreed.find({temperament: { $regex: req.params.temperament, $options: 'i' }})
+  .then(dogBreeds => {
     res.json(dogBreeds);
   });
 });
 
-// create dog
-app.post('/dogs/create', (req, res) => {
-  Dog.create(req.body).then(dog => {
-    res.json(dog);
-  });
-});
-
+/* DOGS routes */
 // get all dogs
 app.get('/dogs', (req, res) => {
-  Dog.find({}).then(dogs => {
+  Dog.find({})
+  .then(dogs => {
     res.json(dogs);
   });
 });
 
-// update dog
-app.put('/dogs/update/:id', (req, res) => {
-  Dog.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(dog => {
-    res.json(dog);
-  });
-});
-
-// delete dog
-app.delete('/dogs/delete/:id', (req, res) => {
-  Dog.findOneAndDelete({ _id: req.params.id }).then(dog => {
-    res.json(dog);
-  });
-});
-
+/* USERS routes */
 // create user
 app.post('/users/create', (req, res) => {
   User.find({ username: req.body.username })
@@ -77,24 +57,88 @@ app.post('/users/create', (req, res) => {
     }
   });
 });
-
-// get user
+// get user by username & password
 app.get('/users/:username/:password', (req, res) => {
-  User.find({ username: req.params.username, password: req.params.password }).then(user => {
+  User.find({ username: req.params.username, password: req.params.password })
+  .then(user => {
     res.json(user);
   });
 });
-
+// get user by dog id
+app.get('/users/:dogid', (req, res) => {
+  User.find({ dogs: { $elemMatch: { _id: req.params.dogid }}})
+  .then(user => {
+    res.json(user);
+  });
+});
 // update user
 app.put('/users/update/:id', (req, res) => {
-  User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(user => {
+  User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+  .then(user => {
     res.json(user);
   });
 });
-
+// update user - create dog within user & within dogs collection
+app.post('/users/:id/dogs/create', (req, res) => {
+  User.findOneAndUpdate({ _id: req.params.id }, { $push: { dogs: req.body }}, { new: true })
+  .then(user => {
+    let dog = {};
+    dog.breed = user.dogs[user.dogs.length - 1].breed;
+    dog._id = user.dogs[user.dogs.length - 1]._id;
+    dog.petName = user.dogs[user.dogs.length - 1].petName;
+    dog.age = user.dogs[user.dogs.length - 1].age;
+    dog.energy_level = user.dogs[user.dogs.length - 1].energy_level;
+    Dog.create(dog)
+    .then(dog => {
+      res.json(dog);
+    });
+  })
+  .catch(err => {
+    res.send(err);
+  });
+});
+// update user - update dog within user & within dogs collection
+app.put('/users/:id/dogs/update/:dogid', (req, res) => {
+  let dogg = {};
+  dogg.breed = req.body.breed;
+  dogg._id = req.params.dogid;
+  dogg.petName = req.body.petName;
+  dogg.age = req.body.age;
+  dogg.energy_level = req.body.energy_level;
+  User.findOneAndUpdate({ _id: req.params.id, "dogs._id": req.params.dogid }, { $set: { "dogs.$" : dogg }}, { new: true })
+  .then(user => {
+    let dog = {};
+    dog.breed = req.body.breed;
+    dog._id = req.params.dogid;
+    dog.petName = req.body.petName;
+    dog.age = req.body.age;
+    dog.energy_level = req.body.energy_level;
+    Dog.findOneAndUpdate({ _id: req.params.dogid }, dog, { new: true })
+    .then(dog => {
+      res.json(dog);
+    });
+  })
+  .catch(err => {
+    res.send(err);
+  });
+});
+// update user - delete dog within user & within dogs collection
+app.delete('/users/:id/dogs/delete/:dogid', (req, res) => {
+  User.findOneAndUpdate({ _id: req.params.id }, { $pull: { dogs: { _id: req.params.dogid } } })
+  .then(user => {
+    Dog.findOneAndDelete({ _id: req.params.dogid })
+    .then(dog => {
+      res.json(dog);
+    })
+  })
+  .catch(err => {
+    res.send(err);
+  });
+});
 // delete user
 app.delete('/users/delete/:id', (req, res) => {
-  User.findOneAndDelete({ _id: req.params.id }).then(user => {
+  User.findOneAndDelete({ _id: req.params.id })
+  .then(user => {
     res.json(user);
   });
 });
